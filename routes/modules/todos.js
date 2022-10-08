b08e8e660047a68776b8e70e6d0ab673d7904879
Howhowchen <debby8698@gmail.com>
2022-10-09 02:05:55 +0800

    feat: add todo.js CRUD

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -2,20 +2,67 @@ const router = require('express').Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => { return res.status(422).json(error) })
+router.get('/new', (req, res) => {
+  res.render('new')
 })
 
-router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
+router.post('/', async (req, res) => {
+  try {
+    const UserId = req.user.id
+    const name = req.body.name
+    await Todo.create({ name, UserId })
+    res.redirect('/')
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const todo = await Todo.findByPk(id)
+    res.render('detail', { todo: todo.toJSON() })
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const UserId = req.user.id
+    const id = req.params.id
+    const todo = await Todo.findOne({ where: { id, UserId } })
+    res.render('edit', { todo: todo.toJSON() })
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const UserId = req.user.id
+    const id = req.params.id
+    const { name, isDone } = req.body
+    const todo = await Todo.findOne({ where: { id, UserId } })
+    todo.name = name
+    todo.isDone = isDone === 'on'
+    await todo.save()
+    res.redirect(`/todos/${id}`)
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const UserId = req.user.id
+    const id = req.params.id
+    const todo = await Todo.findOne({ where: { id, UserId} })
+    await todo.destroy()
+    res.redirect('/')
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 module.exports = router
