ebe1f2d4ab10fbcf96ce75d9e3e5279979a5fbac
Howhowchen <debby8698@gmail.com>
2022-10-09 19:33:12 +0800

    feat: add oauth google login

--- config/passport.js
+++ config/passport.js
@@ -1,55 +1,54 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth2').Strategy
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ where: { email } })
-      .then(user => {
-        if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
-        }
-        return bcrypt.compare(password, user.password).then(isMatch => {
-          if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect.' })
-          }
-          return done(null, user)
-        })
-      })
-      .catch(err => done(err, false))
+  //  passport definition: local
+  passport.use(new LocalStrategy(
+    { 
+      usernameField: 'email', 
+      passReqToCallback: true 
+    }, 
+    async (req, email, password, done) => {
+      try {
+        const user = await User.findOne({ where: { email } })
+        if (!user) return done(null, false, { message: 'That email is not registered!' })
+        
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) return done(null, false, { message: 'Email or Password incorrect.' })
+        
+        return done(null, user)
+      } catch (err) {
+        done(err, false)
+      }
   }))
-  //  Oauth: facebook passport definition
+  //  passport definition: facebook
   passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_ID,
     clientSecret: process.env.FACEBOOK_SECRET,
     callbackURL: process.env.FACEBOOK_CALLBACK,
     profileFields: ['email', 'displayName']
-  }, (accessToken, refreshToken, profile, done) => {
-    const { name, email } = profile._json
-    User.findOne({ where: { email } })
-      .then(user => {
-        if (user) return done(null, user)
-        const randomPassword = Math.random().toString(36).slice(-8)
-        bcrypt
-          .genSalt(10)
-          .then(salt => bcrypt.hash(randomPassword, salt))
-          .then(hash => User.create({
-            name,
-            email,
-            password: hash
-          }))
-          .then(user => done(null, user))
-          .catch(err => done(err, false))
-      })
-  }))
+  },
+    thirdPartyOAuthCallback 
+  ))
+  //  passport definition: google
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK,
+  },
+    thirdPartyOAuthCallback 
+  ))
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
+
   passport.deserializeUser((id, done) => {
     User.findByPk(id)
       .then((user) => {
@@ -57,4 +56,24 @@ module.exports = app => {
         done(null, user)
       }).catch(err => done(err, null))
   })
+}
+
+async function thirdPartyOAuthCallback(accessToken, refreshToken, profile, done) {
+  try {
+    const { name, email } = profile._json
+    const user = await User.findOne({ where: { email } })
+    if (!user) {
+      const randomPassword = Math.random().toString(36).slice(-8)
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(randomPassword, salt)
+      await User.create({
+        name,
+        email,
+        password: hash
+      })
+    }
+    return done(null, user)
+  } catch (err) {
+    return done(err, false)
+  }
 }
